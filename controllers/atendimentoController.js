6604748babccea8e93b2d38f841a920a2108f472
Maycon <maycontomas27@gmail.com>
2026-06-11 18:21:46 -0300

    implementa fluxo completo de atendimento

--- controllers/atendimentoController.js
+++ controllers/atendimentoController.js
@@ -1,8 +1,9 @@
 const db = require('../database/database'); 
 
+
 function adicionarAtendimento(req, res){
 
-
+    let clienteId;
     const nome = req.body.nome;
     const wpp = req.body.wpp;
     const placa = req.body.placa;
@@ -13,6 +14,7 @@ function adicionarAtendimento(req, res){
     const valor = req.body.valor;
     const km = req.body.km;
     const data = req.body.data;
+    
 
 
     db.get(
@@ -27,36 +29,177 @@ function adicionarAtendimento(req, res){
             }
 
             if(cliente){
-                res.send(cliente.id);
-                return;          
-            }
-            
-            console.log("cliente não encontrado!");
-            
-            res.send("cliente não encontrado!");
-            
-            
-            db.run(
-            
-            'INSERT INTO clientes (nome, wpp) VALUES (?, ?)',
-            [nome, wpp],
-            
-            function(erro) {
+                clienteId = cliente.id; 
                 
-                if (erro) {
-                    
-                    res.send('Erro ao criar cliente');
-                    return;
-                }
-                
-                res.send(this.lastID);
+                db.get(
 
+                    'SELECT * FROM carros WHERE placa = ?',
+        [placa],
+        (erro, carro) =>{
+            
+            if(erro){
+                res.send("Erro ao buscar veiculo!");
+                return;
             }
+
+            if(carro){
+                console.log(carro);
+
+                db.run(
+
+    `INSERT INTO servicos
+    (clienteId, servico, km, valor, data, placa)
+    VALUES (?, ?, ?, ?, ?, ?)`,
+
+    [clienteId, servico, km, valor, data, placa],
+
+    (erro) => {
+
+        if(erro){
+            res.send('Erro ao adicionar serviço!');
+            return;
+        };
+
+        res.send('Serviço adicionado!');
+    }
+);
+            }else{
+                console.log("clienteId antes do insert:", clienteId);
+
+                db.run(
+
+                    'INSERT INTO carros(clienteId, placa, marca, modelo, ano) VALUES(?, ?, ?, ?, ?)',
+                    [clienteId, placa, marca, modelo, ano],
+                    (erro) => {
+
+                        if(erro){
+                            res.send("Erro ao adicionar veiculo!");
+                            return;
+                        }
+
+                        
+                        console.log("veiculo incluido no sistema!");
+
+                        db.run(
+
+    `INSERT INTO servicos
+    (clienteId, servico, km, valor, data, placa)
+    VALUES (?, ?, ?, ?, ?, ?)`,
+
+    [clienteId, servico, km, valor, data, placa],
+
+    (erro) => {
+
+        if(erro){
+            res.send('Erro ao adicionar serviço!');
+            return;
+        };
+
+        res.send('Serviço adicionado!');
+    }
+);
+                        
+                    }
+                    
+                );
+            }
+        }
+                );
+
+            } else {
+
+            db.run(
+
+                'INSERT INTO clientes(nome, wpp) VALUES(?, ?)',
+                [nome, wpp],
+                function(erro){
+
+                    if(erro){
+                        res.send("erro ao adicionar cliente!");
+                        return;
+                    }
+
+                    clienteId = this.lastID;
+                    console.log(clienteId);
+                    db.get(
+
+                        'SELECT * FROM carros WHERE placa = ?',
+        [placa],
+        (erro, carro) =>{
+            
+            if(erro){
+                res.send("Erro ao buscar veiculo!");
+                return;
+            }
+
+            if(carro){
+                console.log(carro);
+
+                db.run(
+
+    `INSERT INTO servicos
+    (clienteId, servico, km, valor, data, placa)
+    VALUES (?, ?, ?, ?, ?, ?)`,
+
+    [clienteId, servico, km, valor, data, placa],
+
+    (erro) => {
+
+        if(erro){
+            res.send('Erro ao adicionar serviço!');
+            return;
+        };
+
+        res.send('Serviço adicionado!');
+    }
+);
+            }else{
+                console.log("clienteId antes do insert:", clienteId);
+
+                db.run(
+
+                    'INSERT INTO carros(clienteId, placa, marca, modelo, ano) VALUES(?, ?, ?, ?, ?)',
+                    [clienteId, placa, marca, modelo, ano],
+                    (erro) => {
+
+                        if(erro){
+                            res.send("Erro ao adicionar veiculo!");
+                            return;
+                        }
+
+                        console.log("veiculo incluido no sistema!");
+
+                        db.run(
+
+    `INSERT INTO servicos
+    (clienteId, servico, km, valor, data, placa)
+    VALUES (?, ?, ?, ?, ?, ?)`,
+
+    [clienteId, servico, km, valor, data, placa],
+
+    (erro) => {
+
+        if(erro){
+            res.send('Erro ao adicionar serviço!');
+            return;
+        };
+
+        res.send('Serviço adicionado!');
+    }
+);
+                        
+                    }
+                    
+                );
+            }
+        }
+                    );
+                }
             );
-        }            
+        }   
+        }
     );
 }
-
 
 module.exports = {
     adicionarAtendimento
